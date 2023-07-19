83a46069bb12e89b19cf975ed9671312ae253b06
evin <evinlerouge@icloud.com>
2023-07-19 12:56:54 -0600

    png photos folder added and working

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './styles.css'
+import photo from './photos/logo.png'
 
 const Footer = () => {
   return (
     <footer>
       <p>&copy; {new Date().getFullYear()} Real Estate Company. All rights reserved.</p>
+      <img src={photo} alt="popp"></img>
     </footer>
   );
 };
