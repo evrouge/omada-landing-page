5e30b1093864bdd5ecb3320360f72759afcf2357
evin <evinlerouge@icloud.com>
2023-07-20 12:10:29 -0600

    adding copt

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import './styles.css'
+import image from './photos/logo.png'
 
 const Header = () => {
   return (
+    <>
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/">Omada Real Estate</Navbar.Brand>
+      <Navbar.Brand href="/">Omada Capital Management</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#properties">Properties</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
+          <Nav.Link href="#properties">The Opportunity</Nav.Link>
+          <Nav.Link href="#about">The Team</Nav.Link>
+          <Nav.Link href="#contact">The Profit</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
+ <img class="logos" src={image} alt="logo"></img>
+
+    </>
   );
 };
 
--- src/HeroSection.js
+++ src/HeroSection.js
@@ -1,15 +1,54 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import './styles.css'
+import image from './photos/utah.png'
+import image2 from './photos/icons.png'
+import image3 from './photos/investtimeline.png'
 
 const HeroSection = () => {
   return (
     <>
-      <h1>Omada</h1>
-      <p>
-        Find your dream property today.
-      </p>
+    <div className="component-below">
+      <h1>Omada Capital Management</h1>
+      <h1>Need to put a photo of a house here</h1>
+      <h1>THE OPPORTUNITY</h1>
+        <ul>Why Invest?
+        <li>Low inventory of single-family homes caused home prices to rise</li>
+        <li>Higher interest rates slowed down buyers</li>
+        <li>Sellers won't lower prices of gives up low rates</li>
+        <li>Homes for sale inventory drops</li>
+        <li>More people remain renters for longer</li>
+        <li>This means more income for investment groups who own single-family homes</li>
+        <li>This situation is especially potent in Utah with rapid growth</li>
+        </ul>
+        <img src={image} alt="utah"></img>
+        <div>
+          <h3>Rates will increase or stablize, and prices will continue to stay steady (protecting the equity position). Quality rental applications will increase. Buying during the dip now provides the best of both worlds. </h3>
+        </div>
+        <div>
+          <h2>14%</h2>
+          <h3>Rent Increase*</h3>
+          <p>Rent spikes during the recession and during the boom.</p>
+        </div>
+        <div>
+          <h2>10%</h2>
+          <h3>Property Appreciation*</h3>
+          <p>Bounce back from this dip is likely to increase property values.</p>
+        </div>
+        <div>
+          <h2>COST</h2>
+          <h3>As a Hedge vs Taxes*</h3>
+          <p>Dividents show loss now, but income when property values return.</p>
+        </div>
+        <h1>The Omada Difference</h1>
+        <img src={image2}></img>
+        <img src={image3}></img>
+      <h1>THE TEAM</h1>
+
+      <h1>THE PROFIT</h1>
+
       <Button variant="primary" href="#properties">View Properties</Button>
+      </div>
     </>
   );
 };
