4cac1cdd86c15c43405dbf17a952e98916c46881
evin <evinlerouge@icloud.com>
2023-07-25 13:42:49 -0600

    styling looking good

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -20,9 +20,9 @@ const HeroSection = () => {
 <br/>
 
       <img src={photo} class="qanda"></img>
-      <hr/>
-      <h1 id="opportunity">THE OPPORTUNITY</h1>
-      <hr/>
+
+      <h1 id="opportunity" class="titlestyle">The Opportunity</h1>
+
       <div className='split-container1'>
         <div className='text'>
           <h2>Why invest?</h2>
@@ -36,33 +36,31 @@ const HeroSection = () => {
 </div>
         <img src={image} alt="utah"></img>
 </div>
-<hr/>
-        <div>
+        <div class="rates"><br/>
           <h3>Rates will increase or stablize, and prices will continue to stay steady (protecting the equity position). Quality rental applications will increase. Buying during the dip now provides the best of both worlds. </h3>
-        </div>
-        <div>
           <h2>14%</h2>
           <h3>Rent Increase*</h3>
           <p>Rent spikes during the recession and during the boom.</p>
-        </div>
-        <div>
+
+
           <h2>10%</h2>
           <h3>Property Appreciation*</h3>
           <p>Bounce back from this dip is likely to increase property values.</p>
-        </div>
-        <div>
+
+
           <h2>COST</h2>
           <h3>As a Hedge vs Taxes*</h3>
-          <p>Dividents show loss now, but income when property values return.</p>
-        </div>
-        <br/><br/><hr/>
+          <p>Dividents show loss now, but income when property values return.</p><br/>
+          </div>
+
+        <br/><br/>
         <h1>The Omada Difference</h1><br/>
         <img src={image2} class='qanda'></img>
-        <br/><br/><hr/><br/>
+        <br/><br/><br/>
         <img src={image3} class="qanda"></img>
-        <hr/>
-      <h1 id="team">THE TEAM</h1>
-      <hr/>
+<div class='teamcolor'>
+      <h1 id="team" class="titlestyle">The Team</h1>
+
       <h2>Experience Makes A Difference</h2>
       <h3>Founded by the leading real estate team in Utah for the last 20 years.</h3>
       <ul>
@@ -74,15 +72,18 @@ const HeroSection = () => {
       <p>Investing with OMADA is simpler, more convenient, and potentially more profitable compared to buying investment property directly. It’s a great option for those who want to invest in real estate but don't have the time, money, or expertise to manage properties on their own.</p>
       <p>Investing in the fund provides diversification and reduces risk as your money is spread across multiple properties. And it's more accessible and affordable than buying an individual investment property.</p>
       <p>Would it make sense to have a passive investment that doesn’t require much time or effort–and where you receive regular payouts based on your percentage of the fund?</p>
-     <hr/>
+</div>
       <img src={image4} class="qanda"></img>
-<hr/>
-      <h1 id="profit">THE PROFIT</h1>
-      <hr/>
+      <div class="rates">
+      <h1 id="profit" class='titlestyle'>The Profit</h1>
+
+
+
+      <p>OMADA invests in real estate for regular people to get a good return on their investment. You invest (buy shares) in OMADA, and they pay you quarterly dividends based on the percentage of the investment that is yours.</p>
       <div className="split-container1">
       <h2>How You Get Paid</h2>
       <div className="text">
-      <p>OMADA invests in real estate for regular people to get a good return on their investment. You invest (buy shares) in OMADA, and they pay you quarterly dividends based on the percentage of the investment that is yours.</p>
+
       <h3>1.</h3>
       <p>Rents collected are paid out on a quarterly basis after expenses.*</p>
       <p>Projected Return: 4 - 6% annually</p>
@@ -95,8 +96,9 @@ const HeroSection = () => {
       <p>Projected Annual Return: 7-10%</p>
       </div>
       </div>
+      </div>
       <br/>
-      <hr/>
+
       <h2>Q & A</h2>
       <img src={image5} className="qanda"></img>
       </div>
