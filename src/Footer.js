327998695c4cf818f9baea8f9930b7c213f7e43c
evin <evinlerouge@icloud.com>
2023-07-25 12:27:14 -0600

    fixing logo at top

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer><br/><hr/><br/>
 
       <form class="form-container">
-        <h1>Contact Us!</h1>
+        <h1>Contact Us!</h1><br/>
         Name: <input placeholder='First and Last Name'></input><br/><br/>
         Email: <input placeholder='Email'></input><br/><br/>
         Message: <textarea placeholder='Message Here'></textarea><br/><br/>
