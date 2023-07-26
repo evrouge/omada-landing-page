e0463010731482176796d8ed6d5c27fee57bce12
evin <evinlerouge@icloud.com>
2023-07-26 10:15:49 -0600

    cleaning up and adding spacing where needed

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -21,11 +21,10 @@ const HeroSection = () => {
 
       <img src={photo} class='helloletter'></img>
 
-      <h1 id="opportunity" class="titlestyle">The Opportunity</h1>
-
+      <h1 id="opportunity" class="titlestyle">The Opportunity</h1><br/>
       <div className='split-container1'>
         <div className='text'>
-          <h2>Why invest?</h2>
+          <h2>Why invest?</h2><br/>
         <p>Low inventory of single-family homes caused home prices to rise</p>
         <p>Hgher interest rates slowed down buyers</p>
         <p>Sellers won't lower prices of gives up low rates</p>
@@ -34,30 +33,25 @@ const HeroSection = () => {
         <p>This means more income for investment groups who own single-fampy homes</p>
         <p>This situation is especially potent in Utah with rapid growth</p>
 </div>
-        <img src={image} alt="utah"></img>
+        <img src={image} alt="utah"></img><br/><br/>
 </div>
         <div class="rates"><br/>
           <h3>Rates will increase or stablize, and prices will continue to stay steady (protecting the equity position). Quality rental applications will increase. Buying during the dip now provides the best of both worlds. </h3>
           <h2>14%</h2>
           <h3>Rent Increase*</h3>
           <p>Rent spikes during the recession and during the boom.</p>
-
-
           <h2>10%</h2>
           <h3>Property Appreciation*</h3>
           <p>Bounce back from this dip is likely to increase property values.</p>
-
-
           <h2>COST</h2>
           <h3>As a Hedge vs Taxes*</h3>
           <p>Dividents show loss now, but income when property values return.</p><br/>
           </div>
-
-        <br/><br/>
+        <br/>
         <h1>The Omada Difference</h1><br/>
         <img src={image2} class='qanda'></img>
-        <br/><br/><br/>
-        <img src={image3} class="qanda"></img>
+        <br/><br/><hr/><br/>
+        <img src={image3} class="qanda"></img><br/><br/>
 <div class='teamcolor'>
       <h1 id="team" class="titlestyle">The Team</h1>
 
@@ -71,9 +65,9 @@ const HeroSection = () => {
       <h2>More Profitable</h2>
       <p>Investing with OMADA is simpler, more convenient, and potentially more profitable compared to buying investment property directly. It’s a great option for those who want to invest in real estate but don't have the time, money, or expertise to manage properties on their own.</p>
       <p>Investing in the fund provides diversification and reduces risk as your money is spread across multiple properties. And it's more accessible and affordable than buying an individual investment property.</p>
-      <p>Would it make sense to have a passive investment that doesn’t require much time or effort–and where you receive regular payouts based on your percentage of the fund?</p>
+      <p>Would it make sense to have a passive investment that doesn’t require much time or effort–and where you receive regular payouts based on your percentage of the fund?</p><br/>
 </div>
-      <img src={image4} class="qanda"></img>
+      <br/><img src={image4} class="qanda"></img><br/><br/>
       <div class="rates">
       <h1 id="profit" class='titlestyle'>The Profit</h1>
 
@@ -93,7 +87,7 @@ const HeroSection = () => {
       <p>Projected Annual Return: 4 - 7%</p>
       <h3>3.</h3>
       <p>After rents and equities are realized: </p>
-      <p>Projected Annual Return: 7-10%</p>
+      <p>Projected Annual Return: 7-10%</p><br/>
       </div>
       </div>
       </div>
