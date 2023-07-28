910d3c777119ead53d924a646263bdc9dbebfd4b
evin <evinlerouge@icloud.com>
2023-07-28 13:41:51 -0600

    contact info

--- src/Footer.js
+++ src/Footer.js
@@ -17,7 +17,8 @@ const Footer = () => {
     {/* <div class="image-container">
     <img src={logos} class="bottom-logo"></img>
     </div> */}<br/><br/>
-    <p>Omada Capital Management | 801.555.1234 | omadateam@omada.capital |</p>
+    <h3>Contact Us!</h3>
+    <p>Omada Capital Management | 801.555.1234 | omadateam@omada.capital</p>
     <a href='https://omada.capital'>www.omada.capital</a><br/><br/>
     </>
   );
