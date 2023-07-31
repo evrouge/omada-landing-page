5ae8eb1057fa8b9be321edd0a57ff01afd4baa0d
evin <evinlerouge@icloud.com>
2023-07-31 11:56:06 -0600

    saving

--- src/Footer.js
+++ src/Footer.js
@@ -5,18 +5,7 @@ const Footer = () => {
 
   return (
     <>
-    {/* <footer><br/><br/><br/>
-      <form class="form-container">
-        <h1>Contact Us!</h1><br/>
-        Name: <input placeholder='First and Last Name'></input><br/><br/>
-        Email: <input placeholder='Email'></input><br/><br/>
-        Message: <textarea placeholder='Message Here'></textarea><br/><br/>
-        <input type="submit" value="Send Message"></input>
-      </form><br/><br/>
-    </footer> */}
-    {/* <div class="image-container">
-    <img src={logos} class="bottom-logo"></img>
-    </div> */}<br/><br/>
+  <br/><br/>
     <h3>Contact Us!</h3>
     <p>Omada Capital Management | 801.555.1234 | omadateam@omada.capital</p>
     <a href='https://omada.capital'>www.omada.capital</a><br/><br/>
